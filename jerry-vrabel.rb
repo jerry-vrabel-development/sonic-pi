2af3290efbcd3f39e8bd05ddde4e7655cbdf71c4
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-07-10 11:18:55 -0500

    now Ive added hiHat - facepalm

--- jerry-vrabel.rb
+++ jerry-vrabel.rb
@@ -2,7 +2,6 @@
 
 # Coded by Jerry Vrabel
 
-
 live_loop :bash do
   play :C1, release: 0.5
   sleep 1
@@ -12,4 +11,9 @@ live_loop :boo do
   sync :bash
   sample :bd_haus
   sleep 0.5
+end
+
+live_loop :hiHat do
+  sample :drum_cymbal_closed
+  sleep 0.25
 end
