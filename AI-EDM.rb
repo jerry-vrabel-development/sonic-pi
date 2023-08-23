e1f70f95b4ea7c1237d1f9eec2a748895916afd7
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-08-23 14:01:58 -0500

    changed samples. added build and started drop

--- AI-EDM.rb
+++ AI-EDM.rb
@@ -15,25 +15,38 @@ end
   sleep 2
 end
 
-# Build-Up (16 bars)
-4.times do
-  sample :drum_snare_soft, amp: 1.5
+# Extended Build-Up (32 bars)
+# Introduce the snare
+8.times do
+  sample :elec_lo_snare, amp: 1.5
   sleep 1
 end
 
-4.times do
-  sample :drum_snare_soft, amp: 1.5
+# Increase the snare speed
+8.times do
+  sample :elec_hi_snare, amp: 1.5
   sleep 0.75
 end
 
-4.times do
-  sample :drum_snare_soft, amp: 1.5
+# Further increase
+8.times do
+  sample :elec_mid_snare, amp: 1.5
   sleep 0.5
 end
 
-# Melodic Build & Effects
-2.times do
-  sample :loop_industrial, rate: 1.25, amp: 0.5
-  sample :ambi_glass_rub, rate: -0.5, amp: 0.6  # This can create tension
+# Atmosphere and tension for the last part of the build
+4.times do
+  sample :perc_snap2, rate: 1.25, amp: 0.6
+  sleep 2
+end
+
+# Drop (32 bars)
+live_loop :bass_drop, sync: :beat do
+  use_synth :subpulse
+  play_pattern_timed [:e2, :e1, :e2, :g2], [0.5, 0.5, 0.5, 0.5], amp: 1.5, cutoff: 100
+end
+
+8.times do
+  sample :loop_tabla, rate: 1.25, amp: 0.6
   sleep 2
 end
